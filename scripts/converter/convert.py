f91643b0763093009c8fa6efb92648fef9e99ca2
Alan Smith <commit@alanwsmith.com>
2022-04-03 18:27:59 -0400

    Initial version

---
+++ scripts/converter/convert.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+
+
+def make_table_from_image(path, width):
+
+    image = Image.open(path)
+    initial_width, initial_height = image.size
+    ratio = initial_height / initial_width
+    height = int(width * ratio)
+    image = image.resize((width, height))
+
+    pixels = list(image.getdata())
+    pixel_rows = [pixels[i * width:(i + 1) * width] for i in range(height)]
+
+    color_rows = []
+
+    for pixel_row in pixel_rows: 
+        new_row = []
+        for pixel in pixel_row:
+            hex_string = ''.join([hex(value)[2:] for value in pixel])
+            new_row.append(f'#{hex_string}')
+        color_rows.append(new_row)
+    print(color_rows)
+
+    with open('index.html', 'w') as _f:
+        _f.write('<table>')
+        for color_row in color_rows:
+            _f.write('<tr>')
+            for color_cell in color_row:
+                _f.write(f'<td width="6" height="6" bgcolor="{color_cell}"></td>')
+            _f.write('</tr>')
+            _f.write("\n")
+        _f.write('</table>')
+
+
+if __name__ == '__main__':
+
+    make_table_from_image(
+        '/Users/alan/Graphics/magritte-son-of-man-1964.jpg',
+        100
+    )
+
+
+
